5f508c55daf7b41c115ac0fefc6297933b84afa5
charlieYuan <youmax210139@gmail.com>
2018-05-13 12:34:24 +0800

    - integrate bootstrap material - add page component blog/home/portfolio/resume and contact - add header component

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
+import { AppRoutingModule } from './app-routing.module';
+
+import { ResumeComponent } from './resume/resume.component';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    AppRoutingModule,
+
+  ],
+  declarations: [
+    ResumeComponent,
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    PortfolioComponent,
+    BlogComponent,
+    ContactComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
