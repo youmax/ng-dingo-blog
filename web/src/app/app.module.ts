c5ce11d39ea66fbb9b875deb1c4bc04a70aced52
charlieYuan <youmax210139@gmail.com>
2018-05-14 10:43:05 +0800

    - add common template module - add portfolio template component

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -2,33 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CommonTemplateModule } from './common/common.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { ResumeComponent } from './resume/resume.component';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+
 import { HomeComponent } from './home/home.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { BlogComponent } from './blog/blog.component';
 import { ContactComponent } from './contact/contact.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     CommonModule,
+    CommonTemplateModule,
     AppRoutingModule,
-
   ],
   declarations: [
     ResumeComponent,
     AppComponent,
-    HeaderComponent,
     HomeComponent,
     PortfolioComponent,
     BlogComponent,
-    ContactComponent,
-    FooterComponent
+    ContactComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ web/src/app/common/portfolio/portfolio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.scss']
+})
+export class TemplatePortfolioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
