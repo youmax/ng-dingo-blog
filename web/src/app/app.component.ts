7784aa196cd3fe00ddfd0b9ad1746a39bda157f3
charlieYuan <youmax210139@gmail.com>
2018-05-14 11:16:49 +0800

    integrate jquery.popper.js via angular-cli

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare const $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    $(document).ready(function () {
+      $('body').bootstrapMaterialDesign();
+    });
+
+  }
+
 }
