30e262b9a88580bc8594c487e7764d0ecaa0db8d
charlieYuan <youmax210139@gmail.com>
2018-06-28 15:24:44 +0800

    - remove production environment and add it to git ignore file - replace single quote with double quote

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,22 +1,31 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', loadChildren: './pages/home/home.module#PageHomeModule' },
-  { path: 'resume', loadChildren: './pages/resume/resume.module#PageResumeModule' },
-  { path: 'portfolio', loadChildren: './pages/portfolio/portfolio.module#PagePortfolioModule' },
-  { path: 'contact', loadChildren: './pages/contact/contact.module#PageContactModule' },
-  { path: 'blog', loadChildren: './pages/blog/blog.module#PageBlogModule' },
-  { path: '**', loadChildren: './pages/notfound/notfound.module#PageNotfoundModule' }
+  { path: "", pathMatch: "full", redirectTo: "home" },
+  { path: "home", loadChildren: "./pages/home/home.module#PageHomeModule" },
+  {
+    path: "resume",
+    loadChildren: "./pages/resume/resume.module#PageResumeModule"
+  },
+  {
+    path: "portfolio",
+    loadChildren: "./pages/portfolio/portfolio.module#PagePortfolioModule"
+  },
+  {
+    path: "contact",
+    loadChildren: "./pages/contact/contact.module#PageContactModule"
+  },
+  { path: "blog", loadChildren: "./pages/blog/blog.module#PageBlogModule" },
+  {
+    path: "**",
+    loadChildren: "./pages/notfound/notfound.module#PageNotfoundModule"
+  }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes)
-  ],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- web/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  API_URI: "http://api.desuga.me",
-  APP_LOCALE: "cn",
-  production: true
-};
